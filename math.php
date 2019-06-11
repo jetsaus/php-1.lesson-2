4688f455f43ae4bd281df951a191829400c50d8d
jetsaus <jetsaus@gmail.com>
2019-06-11 22:15:57 +0800

    4.Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation),где $arg1, $arg2 – значения аргументов, $operation – строка с названием операции. В зависимости от переданного значения операции выполнить одну из арифметических операций (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).

---
+++ math.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Реализация функций четырех математических операций: +, -, *, /
+ */
+// Сложение
+function Addition($x = 0, $y = 0)
+{
+    return ($x + $y);
+}
+// Вычитание
+function Substruction($x = 0, $y = 0)
+{
+    return ($x - $y);
+}
+// Умножение
+function Multiplication($x = 0, $y = 0)
+{
+    return ($x * $y);
+}
+// Деление
+function Division($x = 0, $y = 1)
+{
+    if($y != 0) {
+        return ($x / $y);
+    }
+}
+?>
