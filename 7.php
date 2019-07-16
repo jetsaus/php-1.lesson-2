c3d9a8aeee86b26a60669dc3ff21fed662873309
jetsaus <jetsaus@gmail.com>
2019-07-17 00:04:30 +0800

    20.Правильно расставил скобки {} в условиях if функции formatByCount()

--- 7.php
+++ 7.php
@@ -37,9 +37,15 @@
     {
         $count = abs($count) % 100;
         $lcount = $count % 10;
-        if ($count >= 11 && $count <= 19) return($form3);
-        if ($lcount >= 2 && $lcount <= 4) return($form2);
-        if ($lcount == 1) return($form1);
+        if ($count >= 11 && $count <= 19) {
+            return($form3);
+        }
+        if ($lcount >= 2 && $lcount <= 4) {
+            return($form2);
+        }
+        if ($lcount == 1){
+            return($form1);
+        }
         return $form3;
     }
     
